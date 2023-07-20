aeb038fbbd4a3a06a35747db1c63b48d903d630c
Sana-K84 <54663549+Sana-K84@users.noreply.github.com>
2023-07-20 22:46:55 +0300

    +

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const mn = document.querySelector("#mn");
 const sc = document.querySelector("#sc");
 
 const week = ['San', 'Man', 'Tue', 'Wen', 'Sou', 'Fri', 'Set'];
-
-
 const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 const boxLeft = document.querySelector("#box-left");
 const boxRight = document.querySelector("#box-right");
+
+const dayTo = document.querySelector(".count_day_value");
+const hoursTo = document.querySelector(".count_hours_value");
+const minutesTo = document.querySelector(".count_min_value");
+const secondsTo = document.querySelector(".count_sec_value");
 
 
 setInterval(() => {
@@ -24,8 +27,14 @@ setInterval(() => {
     mn.style.transform = `rotateZ(${(minClock)}deg)`;
     sc.style.transform = `rotateZ(${(secClock)}deg)`;
     let monthNum = date.getMonth();
-    boxRight.innerHTML = `${month[monthNum]} 23`
+    let year = date.getFullYear();
+    boxRight.innerHTML = `${month[monthNum]} ${year - 2000}`
 
-
+    const tisDay = new Date('May 04 2024') - date;
+    console.log(tisDay)
+    dayTo.innerHTML = Math.floor(tisDay / 1000 / 60 / 60 / 24);
+    hoursTo.innerHTML = 22;
+    minutesTo.innerHTML = 22;
+    secondsTo.innerHTML = 22;
 });
 
